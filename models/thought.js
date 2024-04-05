afc1dfe3905c401489b1952c81dbe1acca000410
Mel Mel Mom <melmelmomof4@gmail.com>
2024-04-05 13:18:36 -0400

    moved reaction model into thought model and deleted reaction model as it was not created correctly in the first place

--- models/thought.js
+++ models/thought.js
@@ -1,42 +1,76 @@
-const { Schema, model } = require('mongoose');
-// const formatDate = require('.utils/helpers');
-const { Timestamp } = require('mongodb');
+const { Schema, model, Types } = require('mongoose');
 
+const { moment } = require('moment');
+
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+
+    username: {
+      type: String,
+      required: true,
+    },
+
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a'),
+    }
+  },
+
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false,
+  }
+)
 
 const thoughtSchema = new Schema(
   {
     thoughtText: {
       type: String,
-      required: 'Thought is Required',
-    minlength: 1,
-    maxlength: 280,
+      required: 'Please enter a thought no more than 280 characters',
+      minlength: 1,
+      maxlength: 280,
+    },
+
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (Timestamp) => formatDate(Timestamp)
+    },
+
+    username: {
+      type: String,
+      required: true
+    },
+
   },
-
-  createdAt: {
-  type: Date,
-  default: Date.now,
-  get: ( Timestamp ) => formatDate( Timestamp )
-},
-
-  username: {
-  type: String,
-  required: true
-},
-  // reactions: [reactionSchema],
-},
-{
-toJSON: {
-  virtuals: true,
-    getters: true,
-},
-id: false,
-}
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
 );
 
 thoughtSchema.virtual("reactionCount").get(function () {
   return this.reaction.length;
 });
 
-const thought = model('thought', thoughtSchema)
+const thought = model('thought', thoughtSchema, reactionSchema )
 
 module.exports = thought;
